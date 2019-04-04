05b4c62c21d6893e01841ce035e5f617f8de9563
Giorgi <g_gelashvili4@cu.edu.ge>
2019-04-04 11:08:23 +0400

    duplicates fixed

---
+++ src/modules/company/models/Company.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize'
+
+export default class Company extends Sequelize.Model {
+	static init(sequelize) {
+		return super.init(
+			{
+				name: { type: Sequelize.STRING, allowNull: false },
+				industry_id: { type: Sequelize.INTEGER, allowNull: false },
+				logo: { type: Sequelize.STRING, allowNull: false },
+				identification_code: { type: Sequelize.STRING, allowNull: false }
+			},
+			{ sequelize, underscored: true, timestamps: true }
+		)
+	}
+
+	static associate(models) {
+		this.belongsToMany(models.User, {
+			through: 'company_owners',
+			foreignKey: 'company_id'
+		})
+
+		this.belongsTo(models.Industry)
+	}
+}
